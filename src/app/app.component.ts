74b46cd77897692b037a82367c8e0fb3b7477363
Kamalpreet Chauhan <kamalpreet.chauhan@pisolve.com>
2017-12-29 16:09:52 +0000

    remove canvas and display image inside it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-  @ViewChild('canvas') canvas: ElementRef;
-  private ctx: CanvasRenderingContext2D;
 
   ngOnInit(): void {
-    const canvasElm = this.canvas.nativeElement;
-    canvasElm.width = 200;
-    canvasElm.height = 200;
-
-    this.ctx = canvasElm.getContext('2d');
-
-    const image = new Image();
-    image.src = 'https://thetuitionteacher.com/blog/wp-content/uploads/2016/04/business-progress-graphic.jpg';
-
-    image.onload = () => {
-      image.height = 200;
-      image.width = 200;
-      this.ctx.drawImage(image, 0, 0, 200, 200);
-    };
   }
 }
