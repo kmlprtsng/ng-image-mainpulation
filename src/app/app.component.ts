ce72cec6325136ea225fde9bd60a4424c8123ed9
Kamalpreet Chauhan <kamalpreet.chauhan@pisolve.com>
2017-12-29 14:48:59 +0000

    display canvas with black rectangle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('canvas') canvas: ElementRef;
+  private ctx: CanvasRenderingContext2D;
+
+  ngOnInit(): void {
+    const canvasElm = this.canvas.nativeElement;
+    canvasElm.width = 200;
+    canvasElm.height = 200;
+
+    this.ctx = canvasElm.getContext('2d');
+    this.ctx.fillStyle = 'e2e2e2';
+    this.ctx.fillRect(0, 0, 200, 200);
+    // let image = new Image();
+
+  }
 }
