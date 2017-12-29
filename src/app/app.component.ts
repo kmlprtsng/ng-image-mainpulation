9256a1c4c041673b23e43a16ef6d7b0419c7f562
Kamalpreet Chauhan <kamalpreet.chauhan@pisolve.com>
2017-12-29 16:54:21 +0000

    add move and zoom ability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,34 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
+  x = 100;
+  y = 100;
+  scale = 1;
 
   ngOnInit(): void {
+  }
+
+  moveUp() {
+    this.y--;
+  }
+
+  moveDown() {
+    this.y++;
+  }
+
+  moveLeft() {
+    this.x--;
+  }
+
+  moveRight() {
+    this.x++;
+  }
+
+  zoomIn() {
+    this.scale += 0.025;
+  }
+
+  zoomOut() {
+    this.scale -= 0.025;
   }
 }
